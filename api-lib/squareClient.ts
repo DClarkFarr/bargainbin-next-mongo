644689f4c8eaad3393eb09cce5e2c786ee35d28b
Daniel Farr <daniel@aptivada.com>
2022-09-24 22:56:45 -0600

    console log

--- api-lib/squareClient.ts
+++ api-lib/squareClient.ts
@@ -1,6 +1,5 @@
 import { Client, Environment, ApiError } from "square";
 
-console.log("access token", process.env.SQUARE_ACCESS_TOKEN);
 const client = new Client({
     accessToken: process.env.SQUARE_ACCESS_TOKEN,
     environment: process.env.SQUARE_ENV as Environment,
